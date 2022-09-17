82a7622d0d898bfcc3a8b654816ef4c93c05a1f6
Azimka <71658005+Azimka228@users.noreply.github.com>
2022-09-17 21:16:07 +0300

    Refactor files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
 import Body from "./components/Body/Body";
-import NewComponent from "./components/NewComponent";
+import NewComponent from "./components/NewComponent/NewComponent";
 import Button from "./components/Button/button";
 import UseState from "./components/useState/useState";
 
